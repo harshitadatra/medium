e52d3447e9d380bb32aa3bbb2adb3ad2b11a052f
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-07-16 01:42:46 +0530

    feat: added guest credentials

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -12,11 +12,17 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     password: "",
   });
 
-  async function sendRequest() {
+  async function sendRequest(isGuest: boolean) {
     try {
       const response = await axios.post(
         `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
-        postInputs
+        isGuest
+          ? {
+              name: "Test User",
+              username: "test_user@gmail.com",
+              password: "123456",
+            }
+          : postInputs
       );
       const jwt = response.data;
       localStorage.setItem("token", jwt);
@@ -80,12 +86,21 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
               }}
             />
             <button
-              onClick={sendRequest}
+              onClick={() => sendRequest(false)}
               type="button"
               className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
               {type === "signup" ? "Sign up" : "Sign in"}
             </button>
+            {type === "signin" ? (
+              <button
+                onClick={() => sendRequest(true)}
+                type="button"
+                className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                {"Use Guest Credentials"}
+              </button>
+            ) : null}
           </div>
         </div>
       </div>
